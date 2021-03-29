f0f4e02f09dbe9cd92917af1ce790d4758a1957d
Kossam Ouma <koss797@gmail.com>
2021-03-29 16:12:49 +0000

    Made the Jobs post on user command

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from bs4 import BeautifulSoup
 import requests
 from discord.ext import commands
 from keep_alive import keep_alive
-# import lxml
+import lxml
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix=".")
 
-try:
-  import lxml
-except:
-  os.system('pip install lxml')
+# try:
+#   import lxml
+# except:
+#   os.system('pip install lxml')
 
 @client.event
 async def on_ready():
@@ -23,30 +23,27 @@ async def on_ready():
             type=discord.ActivityType.listening, name="Fuzu Job Posts"))
     print("Bot's Ready")
 
-    while True:
-        # os.system('pip install lxml')
-        dev_job_channel = discord.utils.get(client.get_all_channels(), name="💼-dev-job-opportunities")
+@client.command()
+async def jobs(ctx):
+    # os.system('pip install lxml')
+    dev_job_channel = discord.utils.get(client.get_all_channels(), name="💼-dev-job-opportunities")
 
-        html_text = requests.get('https://www.fuzu.com/categories/it-software').text
-        soup = BeautifulSoup(html_text, 'lxml')
-        jobs = soup.find_all('div', class_='slim-card mb-2 job-card-padding')
+    html_text = requests.get('https://www.fuzu.com/categories/it-software').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('div', class_='slim-card mb-2 job-card-padding')
 
-        embed = discord.Embed(
-                    title='FUZU JOBS',
-                    description='ICT and Software Category',
-                    color=discord.Color.green())
-        for job in jobs:
-            title = job.find('h3', class_='font-18 slim-titles job-titles').text
-            more_info = "https://www.fuzu.com" + job.find('a', class_='jobs-button continue-job desktop')['href']
+    embed = discord.Embed(
+                title='FUZU JOBS',
+                description='ICT and Software Category',
+                color=discord.Color.green())
+    for job in jobs:
+        title = job.find('h3', class_='font-18 slim-titles job-titles').text
+        more_info = "https://www.fuzu.com" + job.find('a', class_='jobs-button continue-job desktop')['href']
 
-            embed.add_field(name=title, value=more_info, inline=False)
+        embed.add_field(name=title, value=more_info, inline=False)
 
-        time_wait = 10080
-        embed.set_footer(text="This refreshes every 7 days")
-        await dev_job_channel.send(embed=embed)
-        
-        print(f'Waiting {time_wait} minutes...')
-        time.sleep(time_wait * 60)
+    embed.set_footer(text="This message appears on command")
+    await dev_job_channel.send(embed=embed)
 
 
 keep_alive()
